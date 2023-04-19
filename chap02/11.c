f258678e1678ee2f560f97bca82ba4ff94b08b8d
박선영 <pcadl0196@gmail.com>
2023-04-19 22:28:05 +0900

    230419 c언어 문자열예제까지

---
+++ chap02/11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//문자형 변수 이해하기(char)
+//문자형 변수는 작은 범위으 ㅣ값을 저장하기 위한 변수형
+main()
+{
+    char ch;
+    unsigned char j;
+    char k;
+
+    ch = 200;
+    j = 200;
+    k = 'a';
+
+    printf("문자형 변수 ch의 값은 %d \n", ch);
+    printf("문자형 변수 j의 값은 %d \n", j);
+    printf("문자형 변수 k의 값은 %d \n", k);
+}
