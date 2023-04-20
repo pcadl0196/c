5277a33de603d4c3eb127e3953e865a3614b094b
박선영 <pcadl0196@gmail.com>
2023-04-20 22:31:32 +0900

    230420 논리형 변수 이해까지 예제 작성

---
+++ chap02/12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//정수형 변수 이해하기
+main() 
+{
+    int i;
+    unsigned int j;
+    int k;
+
+    i = 20000000000;
+    j = 40000000000;
+    k = 'b';
+
+    printf("정수형 변수 i의 값은 %d \n", i);
+    printf("정수형 변수 j의 값은 %u \n", j);
+    printf("정수형 변수 k의 값은 %d \n", k);
+}
